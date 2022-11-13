8517b1953ad3bc56482aec9ae56379a1c75fc2f2
MoeedCh <moeedchaudhry15@gmail.com>
2022-11-13 06:11:22 -0500

    updated testGMPLot

--- Testing/testGMPLOT.py
+++ Testing/testGMPLOT.py
@@ -1,6 +1,7 @@
 import gmaps
 from Interface import PopulateData
 import tkinter
+from tkinter import *
 from tkintermapview import TkinterMapView
 
 def testGPLOT():
@@ -21,11 +22,25 @@ def testGPLOT():
     root_tk.geometry("500x500")
     root_tk.title("Google Maps")
 
+    def ScheduleEvent():
+        print('FuCk')
+        
+    def eventsPopUp():
+        global PopUp
+        PopUp = Toplevel(root_tk)
+        PopUp.title('Event List')
+        PopUp.geometry('400x650')
+
+        popUpLabel = Label(PopUp, text='LIST OF EVENTS')
+
+        ScheduleButton = Button(root_tk, text='Schedule', command=ScheduleEvent)
+        ScheduleButton.grid(row=0, column=1)
+        ScheduleButton.pack(pady=50)
     #map inside window
     map_widget = TkinterMapView(root_tk, width=600,height=400,corner_radius=0)
     map_widget.pack(fill='both',expand=True)
     for i in populate.locationsOnMap:
-        map_widget.set_marker(float(i.latitude), float(i.longitude), text=i.name)
+        map_widget.set_marker(float(i.latitude), float(i.longitude), text=i.name, command=eventsPopUp)
 
     map_widget.set_position(37.220090, -80.422660)
     map_widget.set_zoom(15)
