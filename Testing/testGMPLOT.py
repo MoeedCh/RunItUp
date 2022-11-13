a76db37335426da82bafdf7f739c2b4a6faa5558
MoeedCh <moeedchaudhry15@gmail.com>
2022-11-13 05:50:04 -0500

    added buttons and pop-ups

--- Testing/testGMPLOT.py
+++ Testing/testGMPLOT.py
@@ -1,6 +1,7 @@
 import gmaps
 from Interface import PopulateData
 import tkinter
+from tkinter import *
 from tkintermapview import TkinterMapView
 
 def testGPLOT():
@@ -21,13 +22,29 @@ def testGPLOT():
     root_tk.geometry("500x500")
     root_tk.title("Google Maps")
 
-    def changeColor(marker):
-        localMark = map_widget.set_marker(marker_color_outside='red')
+    def Schedule():
+        print("FUCK")
+
+    def testPop(marker):
+        global pop
+        pop = Toplevel(root_tk)
+        pop.title("Event List")
+        pop.geometry("350x600")
+
+        popLabel = Label(pop, text="LIST OF SCHEDULED EVENTS: ")
+        popLabel.pack(pady=10)
+        myFrame = Frame(pop, bg='green')
+        myFrame.pack(pady=5)
+
+
+        ScheduleEventButton = Button(myFrame, text="Schedule",command=Schedule)
+        ScheduleEventButton.grid(row=0, column=1)
+        ScheduleEventButton.pack(50)
     #map inside window
     map_widget = TkinterMapView(root_tk, width=600,height=400,corner_radius=0)
     map_widget.pack(fill='both',expand=True)
     for i in populate.locationsOnMap:
-        marker = map_widget.set_marker(float(i.latitude), float(i.longitude), text=i.name, command=changeColor)
+        marker = map_widget.set_marker(float(i.latitude), float(i.longitude), text=i.name, command=testPop)
     map_widget.set_position(37.220090, -80.422660)
     map_widget.set_zoom(15)
 
